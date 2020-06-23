668aeb8185b706b0a6c38883c63ed58d6c636018
Tyler Hodzen <38892841+thodzen@users.noreply.github.com>
2020-06-23 15:29:22 +0000

    Add project files.

---
+++ LearningCSharp/TestingBasics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LearningCSharp
+{
+    // Ctrl+R, then A to run all tests
+
+    public class TestingBasics
+    {
+        [Fact] // Use [Fact] to denote a test
+        public void CanAddTwoNumbers()
+        {
+            // Given / Arrange
+            int a = 10, b = 20;
+            // When / Act
+            int answer = a + b;
+            // Then / Assert
+            Assert.Equal(30, answer);
+        }
+
+        [Theory]
+        [InlineData(2,2,4)] // test int a, int b, and int expected
+        [InlineData(5,5,10)]
+        [InlineData(20,5,25)]
+        public void CanAddNumbers(int a, int b, int expected)
+        {
+            int answer = a + b;
+            Assert.Equal(expected, answer);
+        }
+    }
+}
---
+++ LearningCSharp/VariablesAndDataTypes.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Xunit;
+
+namespace LearningCSharp
+{
+    public class VariablesAndDataTypes
+    {
+
+        [Fact]
+        public void SomeOfTheBuiltInTypes() 
+        {
+            // Type Identifier;
+            int age;
+            age = 22;
+            Int32 yourAge;
+            yourAge = 23;
+
+            string name;
+            name = "Tyler";
+            String yourName;
+            yourName = "Tyler2";
+
+            // Initializing - assigning a value when declaring
+            Double pay = 33.22;
+            decimal yourPay = 33.22M;
+
+            Boolean isAwake = true;
+            bool over = 12 > 13;
+
+            int x;
+
+            x = 0;
+            Assert.Equal(0, x);
+        }
+
+        [Fact]
+        public void ImplicitlyTypedLocalVariable()
+        {
+            // You can use var, but you HAVE to initalize it
+            // Can only use var with local variables
+            var pay = 32.50M; // It knows this is a decimal
+            // C# is a strictly typed language.
+            // A variable can never change its data type
+            // Born an integer? You will ALWAYS be an integer
+        }
+
+        [Fact]
+        public void ValueTypes()
+        {
+            // Structs create Stack Value Types
+            // Classes create Heap Reference Types
+            // Reference types are REFERENCED in the heap
+            // Object live on the heap
+
+            //Structs
+            int age = 52;
+            DateTime myBirthday = new DateTime(1969, 4, 20);
+            string myName = "Jeff";
+
+            DoubleIt(age);
+            Assert.Equal(52, age);
+
+            var rover = new Dog();
+            rover.Name = "Rover";
+            DoSomething(rover);
+            Assert.Equal("Fido", rover.Name);
+        }
+
+        [Fact]
+        public void UsingRefAndOut()
+        {
+            // comment: ctrl + k, ctrl + c
+            // uncomment: ctrl + k, ctrl + u
+            int y = 50;
+            DoubleItRef(ref y);
+            Assert.Equal(100, y);
+
+            //int p;
+            //GimmeSomething(out p);
+            GimmeSomething(out int p);
+            Assert.Equal(42, p);
+        }
+
+        public void DoubleItRef(ref int x)
+        {
+            x = x + x;
+        }
+
+        public void GimmeSomething(out int z) 
+        {
+            z = 42;
+        }
+
+        public void DoubleIt(int age)
+        {
+            age = age + age;
+        }
+
+        public void DoSomething(Dog d)
+        {
+            d.Name = "Fido";
+        }
+
+    }
+
+    public class Dog {
+        public string Name;
+    }
+}
